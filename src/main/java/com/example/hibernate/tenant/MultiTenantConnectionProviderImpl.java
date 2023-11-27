7cf59797b74956205952071ae7e3af8ce87acb6a
wangyanchao <1558490570@qq.com>
2023-11-27 11:45:38 +0800

    ;

--- src/main/java/com/example/hibernate/tenant/MultiTenantConnectionProviderImpl.java
+++ src/main/java/com/example/hibernate/tenant/MultiTenantConnectionProviderImpl.java
@@ -1,21 +1,14 @@
 package com.example.hibernate.tenant;
 
-import com.example.hibernate.dao.TenantRepository;
-import com.example.hibernate.entity.Tenant;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * 多租户连接提供器
@@ -35,36 +28,15 @@ public class MultiTenantConnectionProviderImpl extends AbstractDataSourceBasedMu
     @Qualifier("multiDataSources")
     private Map<String, DataSource> dataSources ;
 
-//    @Autowired
-//    private TenantRepository tenantRepository;
-
     @Override
     protected DataSource selectAnyDataSource() {
-        return dataSources.values().iterator().next();
+        DataSource dataSource = dataSources.values().iterator().next();
+        return dataSource;
     }
 
     @Override
     protected DataSource selectDataSource(String tenantIdentifier) {
         DataSource dataSource = dataSources.get(tenantIdentifier);
-//        if (dataSource == null) {
-//            Tenant tenant = tenantRepository.findByTenantId(tenantIdentifier);
-//            if (tenant !=null) {
-//                HikariConfig config = new HikariConfig();
-//                config.setJdbcUrl("jdbc:mysql://localhost:3306/" + tenant.getName() + "?useSSL=false&useUnicode=true&characterEncoding=utf-8&allowPublicKeyRetrieval=true&serverTimezone=Asia/Shanghai");
-//                config.setUsername("root");
-//                config.setPassword("123");
-//                config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//                config.setMinimumIdle(1);
-//                config.setMaximumPoolSize(5);
-//                config.setIdleTimeout(60000);
-//                config.setConnectionTimeout(30000);
-//                dataSource = new HikariDataSource(config);
-//                dataSources.put(tenantIdentifier, dataSource);
-//                logger.info("Created DataSource for tenant '{}'", tenantIdentifier);
-//            } else {
-//                logger.warn("Tenant '{}' not found", tenantIdentifier);
-//            }
-//        }
         return dataSource;
     }
 }
